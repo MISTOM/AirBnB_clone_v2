736c67e2a28dab809c3264feb3e75a522f8065f1
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2022-02-02 20:30:31 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 """
-Starts a Flas web application
+starts a Flask web application
 """
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
-    """Returns Hello HBNH!"""
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
