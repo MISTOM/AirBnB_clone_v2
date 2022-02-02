3a496d6a5c81d25755008586eaa5c167e388c67b
Kigarde <kigardetom2001@gmail.com>
2022-02-02 17:23:42 +0000

    add documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/urs/bin/python3
+"""
+Starts a Flas web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    return 'Hello HBNB'
+    """Returns Hello HBNH!"""
+    return 'Hello HBNB!'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
